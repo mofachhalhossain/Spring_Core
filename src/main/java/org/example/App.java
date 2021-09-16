3e7c5bb1f13c6335ab7ffe94e59d3463f981ba1a
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-09-16 15:34:22 +0600

    premetive data type finished

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,8 +11,12 @@ public class App
 
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 
-        EmployeeBeen employee1 = (EmployeeBeen) context.getBean("employee");
+        EmployeeBeen employee1 = (EmployeeBeen) context.getBean("employee1");
+        EmployeeBeen employee2 = (EmployeeBeen) context.getBean("employee2");
+        EmployeeBeen employee3 = (EmployeeBeen) context.getBean("employee3");
 
         System.out.println(employee1);
+        System.out.println(employee2);
+        System.out.println(employee3);
     }
 }
