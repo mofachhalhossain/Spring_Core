cf0d11fd881f2c2ec6b957e568c3d43e47385421
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-09-16 14:41:49 +0600

    bean called

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,18 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        EmployeeBeen employee1 = (EmployeeBeen) context.getBean("employee");
+
+        System.out.println(employee1);
     }
 }
